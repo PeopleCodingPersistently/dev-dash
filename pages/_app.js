5b77425d6c087acc3b17ec593abd30433212d86d
Zach Butler <zach.standley93@gmail.com>
2020-12-22 20:38:51 -0800

    6 setup auth 0 (#13)
    * added auth0 package
    
    * some auth
    
    * deleted unused variable
    
    Co-authored-by: cosmolightfoot <cosmolightfoot@gmail.com>

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
-import '../styles/globals.css';
+import { Auth0Provider } from 'use-auth0-hooks';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <Auth0Provider
+      domain={process.env.NEXT_PUBLIC_AUTH0_DOMAIN}
+      clientId={process.env.NEXT_PUBLIC_AUTH0_CLIENT_ID}
+      redirectUri={`${process.env.NEXT_PUBLIC_BASE_SERVER_URL}`}
+    >
+      <Component {...pageProps} />
+    </Auth0Provider>
+  );
 }
 
 export default MyApp;
---
+++ pages/authenticated/index.js
@@ -0,0 +1,3 @@
+export default function AuthenticatedPage() {
+  return <h1>You are authenticated</h1>;
+}
