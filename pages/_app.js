fc1e048917c58191bb890f9d9332b7ff3848432a
cosmolightfoot <cosmolightfoot@gmail.com>
2020-12-22 19:37:10 -0800

    added auth0 package

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,18 @@
-import '../styles/globals.css';
+import "../styles/globals.css";
+import { Auth0Provider } from "use-auth0-hooks";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  console.log('')
+
+  return (
+    <Auth0Provider
+      domain={process.env.AUTH0_DOMAIN}
+      clientId={process.env.AUTH0_CLIENT_ID}
+      redirectUri={`${process.env.BASE_SERVER_URL}/authenticated`}
+    >
+      <Component {...pageProps} />
+    </Auth0Provider>
+  );
 }
 
 export default MyApp;
